ee506d33c602f76142dcaa4f5fcb8a8afa9d31e0
Ruben Schmittel <151785420+Rubi007007@users.noreply.github.com>
2024-10-04 00:29:52 +0200

    finished first version of about me

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, AboveTheFoldComponent, AboutMeComponent],
+  imports: [CommonModule, AboveTheFoldComponent, AboutMeComponent, SkillsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
