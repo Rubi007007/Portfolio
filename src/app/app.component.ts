adb50b296f88c5775d02314af36cd58cb7fe8121
Ruben Schmittel <151785420+Rubi007007@users.noreply.github.com>
2024-10-03 21:10:55 +0200

    changed navbar to header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/footer/footer.component';
 import { MainContentComponent } from "./main-content/main-content.component";
-import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent, MainContentComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
