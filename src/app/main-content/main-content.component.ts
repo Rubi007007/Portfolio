f9d32918bf40df52f91707b0cdd73d6dd80374ac
Ruben Schmittel <151785420+Rubi007007@users.noreply.github.com>
2024-10-03 23:40:33 +0200

    about me section added

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, AboveTheFoldComponent],
+  imports: [CommonModule, AboveTheFoldComponent, AboutMeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
