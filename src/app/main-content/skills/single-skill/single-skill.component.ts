9f3d311dd1d36c305052b0a96d8b1fa4a95b004c
Ruben Schmittel <151785420+Rubi007007@users.noreply.github.com>
2024-10-06 18:20:54 +0200

    Finished Skill-Section

--- src/app/main-content/skills/single-skill/single-skill.component.ts
+++ src/app/main-content/skills/single-skill/single-skill.component.ts
@@ -1,17 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-single-skill',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './single-skill.component.html',
   styleUrl: './single-skill.component.scss'
 })
 export class SingleSkillComponent {
   @Input()skill = {
     name: "HTML",
-    img: "html-icon.png"
+    img: "html-icon.png",
+    number: 1
   }
 
   inputData = "";
--- src/app/skills.service.ts
+++ src/app/skills.service.ts
@@ -12,46 +12,57 @@ export class SkillsdataService {
         {
             name: "HTML",
             img:"html-icon.png",
+            number: 1
         },
         {
             name: "CSS",
             img:"css-icon.png",
+            number: 2
         },
         {
             name: "JavaScript",
             img:"js-icon.png",
+            number: 3
         },
         {
             name: "TypeScript",
             img:"ts-icon.png",
+            number: 4
         },
         {
             name: "Angular",
             img:"angular-icon.png",
+            number: 5
         },
         {
             name: "Firebase",
             img:"firebase-icon.png",
+            number: 6
         },
         {
             name: "GIT",
             img:"git-icon.png",
+            number: 7
         },
         {
             name: "Rest-Api",
             img:"api-icon.png",
+            number: 8
         },
         {
             name: "Scrum",
             img:"scrum-icon.png",
+            number: 9
         },
         {
             name: "Material Design",
             img:"md-icon.png",
+            number: 10
         },
         {
             name: "Continually learning",
             img:"learning-icon.png",
+            number: 11
         },
     ]
 }
