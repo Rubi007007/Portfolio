3f4945a0db4ae15452bed461955e0b3c74da336e
Ruben Schmittel <151785420+Rubi007007@users.noreply.github.com>
2024-10-08 21:38:59 +0200

    added contact me section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -4,11 +4,12 @@ import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
 import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, AboveTheFoldComponent, AboutMeComponent, SkillsComponent, PortfolioComponent],
+  imports: [CommonModule, AboveTheFoldComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, ContactComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
