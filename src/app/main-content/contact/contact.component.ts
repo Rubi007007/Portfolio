68cbd1c7b6b23216efd9dd54e97a631717eca6e4
Ruben Schmittel <151785420+Rubi007007@users.noreply.github.com>
2024-10-08 22:40:55 +0200

    added a bit logic to form

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  @ViewChild('name') nameInput!: ElementRef;
+  isNameInvalid: boolean = false;
 
+
+  checkValue() {
+    const nameValue = this.nameInput.nativeElement.value;
+    
+    if (nameValue === '') {
+      this.isNameInvalid = true;
+    } else {
+      this.isNameInvalid = false;
+    }
+  }
 }
