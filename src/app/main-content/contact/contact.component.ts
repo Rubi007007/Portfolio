68126f0a12bb51630d59134ddecf5d5211d3f22e
Ruben Schmittel <151785420+Rubi007007@users.noreply.github.com>
2024-10-09 15:20:35 +0200

    sending mail is now working

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -7,7 +7,7 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
   @ViewChild('name') nameInput!: ElementRef;
@@ -23,20 +23,62 @@ export class ContactComponent {
   isMessageValid: boolean = false;
   isCheckboxChecked: boolean = false;
   isFormValid: boolean = false;
+  isMailSent: boolean = false;
+  isVisible: boolean = false;
 
   contactData = {
     name: '',
     email: '',
     message: '',
-  }
+  };
 
-  onSubmit() {
-    console.log('test')
+  onSubmit(event: Event) {
+    event.preventDefault();
+
+    const form = event.target as HTMLFormElement;
+    if (!form) {
+      console.error('Form not found');
+      return;
+    }
+
+    const data = new FormData(form);
+
+    fetch('https://formspree.io/f/xdkonjgv', {
+      method: 'POST',
+      body: data,
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+      .then(() => {
+        this.isMailSent = true;
+        this.isVisible = true;
+
+        document.body.classList.add('no-scroll');
+
+        setTimeout(() => {
+          this.isMailSent = false;
+          this.isVisible = false;
+
+          document.body.classList.remove('no-scroll');
+
+          window.scrollTo(0, 0);
+
+          window.onbeforeunload = function() {
+            window.scrollTo(0, 0);
+          };
+
+          location.reload();
+        }, 3000);
+      })
+      .catch((error) => {
+        console.error('Error submitting the form', error);
+      });
   }
 
   checkValueName() {
     const nameValue = this.nameInput.nativeElement.value;
-    
+
     if (nameValue === '') {
       this.isNameInvalid = true;
     } else {
@@ -48,7 +90,7 @@ export class ContactComponent {
 
   checkValidName() {
     const nameValue = this.nameInput.nativeElement.value;
-    
+
     if (nameValue === '') {
       this.isNameValid = false;
       this.isNameInvalid = true;
@@ -96,7 +138,7 @@ export class ContactComponent {
     } else {
       this.isMessageInvalid = false;
     }
-    
+
     this.checkFormValidity();
   }
 
@@ -120,6 +162,14 @@ export class ContactComponent {
   }
 
   checkFormValidity() {
-    this.isFormValid = this.isNameValid && this.isMailValid && this.isMessageValid && this.isCheckboxChecked;
+    this.isFormValid =
+      this.isNameValid &&
+      this.isMailValid &&
+      this.isMessageValid &&
+      this.isCheckboxChecked;
+  }
+
+  goToTop() {
+    window.scrollTo(0, 0);
   }
 }
