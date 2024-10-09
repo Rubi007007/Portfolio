fcf1c097949d23f1a7c38066aa5e2018be59bf86
Ruben Schmittel <151785420+Rubi007007@users.noreply.github.com>
2024-10-09 15:36:53 +0200

    AOS animations implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/footer/footer.component';
 import { MainContentComponent } from "./main-content/main-content.component";
 import { HeaderComponent } from './shared/header/header.component';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,14 @@ import { HeaderComponent } from './shared/header/header.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'portfolio';
+
+  ngOnInit() {
+    AOS.init({
+      duration: 1000, // Dauer der Animation in Millisekunden
+      once: true, // Animation nur einmal abspielen
+      mirror: false // Animationen beim Scrollen nach oben abspielen
+    });
+  }
 }
