5ace998740c240da5450f8d92f9f5a1bc03c9c41
Ruben Schmittel <151785420+Rubi007007@users.noreply.github.com>
2024-10-09 07:08:09 +0200

    correct input

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -11,16 +11,83 @@ import { FormsModule } from '@angular/forms';
 })
 export class ContactComponent {
   @ViewChild('name') nameInput!: ElementRef;
+  @ViewChild('email') mailInput!: ElementRef;
+  @ViewChild('message') messageInput!: ElementRef;
   isNameInvalid: boolean = false;
+  isNameValid: boolean | undefined;
+  
+  isMailInvalid: boolean = false;
+  isMailValid: boolean = false;
+
+  isMessageInvalid: boolean = false;
+  isMessageValid: boolean = false;
 
 
-  checkValue() {
+  checkValueName() {
     const nameValue = this.nameInput.nativeElement.value;
     
     if (nameValue === '') {
       this.isNameInvalid = true;
     } else {
       this.isNameInvalid = false;
+    }
+  }
+
+  checkValidName() {
+    const nameValue = this.nameInput.nativeElement.value;
+    
+    if (nameValue === '') {
+      this.isNameValid = false;
+      this.isNameInvalid = true;
+    } else {
+      this.isNameValid = true;
+      this.isNameInvalid = false;
+    }
+  }
+
+  checkValueMail() {
+    const mailValue = this.mailInput.nativeElement.value;
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (mailValue === '' || !emailPattern.test(mailValue)) {
+      this.isMailInvalid = true;
+    } else {
+      this.isMailInvalid = false;
+    }
+  }
+
+  checkValidMail() {
+    const mailValue = this.mailInput.nativeElement.value;
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (mailValue === '' || !emailPattern.test(mailValue)) {
+      this.isMailInvalid = true;
+      this.isMailValid = false;
+    } else {
+      this.isMailInvalid = false;
+      this.isMailValid = true;
+    }
+  }
+
+  checkValueMessage() {
+    const messageValue = this.messageInput.nativeElement.value;
+
+    if (messageValue === '') {
+      this.isMessageInvalid = true;
+    } else {
+      this.isMessageInvalid = false;
+    }
+  }
+
+  checkValidMessage() {
+    const messageValue = this.messageInput.nativeElement.value;
+
+    if (messageValue === '') {
+      this.isMessageInvalid = true;
+      this.isMessageValid = false;
+    } else {
+      this.isMessageInvalid = false;
+      this.isMessageValid = true;
     }
   }
 }
