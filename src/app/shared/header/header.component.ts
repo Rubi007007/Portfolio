04c866e76e783b35c2e5950baf3d568d67f0db14
Ruben Schmittel <151785420+Rubi007007@users.noreply.github.com>
2024-10-18 20:39:53 +0200

    Overlay mobile menu added

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  isOverlayVisible = false;
+  isClosing = false; // Variable für den Schließzustand
 
+  constructor(private renderer: Renderer2) {}
+
+  toggleOverlay() {
+    this.isOverlayVisible = !this.isOverlayVisible;
+    if (this.isOverlayVisible) {
+      this.renderer.setStyle(document.body, 'overflow', 'hidden');
+    } else {
+      this.closeOverlay();
+    }
+  }
+
+  closeOverlay() {
+    this.isClosing = true; // Schließzustand aktivieren
+
+    // Entferne das Overlay nach der Animationsdauer (500ms) und setze den Zustand zurück
+    setTimeout(() => {
+      this.isOverlayVisible = false;
+      this.isClosing = false;
+      this.renderer.removeStyle(document.body, 'overflow');
+    }, 500); // Zeit muss der Animationsdauer in der CSS entsprechen
+  }
 }
