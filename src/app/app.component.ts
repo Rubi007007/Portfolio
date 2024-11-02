145b60d865f5f383e970c670582218aa5ebf304c
Ruben Schmittel <151785420+Rubi007007@users.noreply.github.com>
2024-11-02 21:22:25 +0100

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,30 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/footer/footer.component';
 import { MainContentComponent } from "./main-content/main-content.component";
 import { HeaderComponent } from './shared/header/header.component';
+import { Router, NavigationEnd } from '@angular/router';
 import AOS from 'aos';
+import { ImprintComponent } from "./imprint/imprint.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, MainContentComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, MainContentComponent, ImprintComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit{
   title = 'portfolio';
+  showBackground: boolean = false;
 
-  ngOnInit() {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showBackground = event.url === '/' || event.url === '/#atf' || event.url === '/#about-me-section' || event.url === '/#skills' || event.url === '/#portfolio'; // Passe '/home' ggf. an die Landing Page an
+      }
+    });
+
     AOS.init({
       duration: 1000,
       once: true,
